181fd4c0653fc483bce78584a85954abfa131d54
João Victor <elietejvc@hotmail.com>
2024-09-03 21:01:48 -0300

    Joquempo Finalizado

--- index.js
+++ index.js
@@ -25,177 +25,142 @@ let scorePlayer1 = 0
 let scorePlayer2 = 0
 let gameStart = false
 
-console.log(movePlayer1)
-console.log(movePlayer2)
-
 function verifyWinner() {
-    if (movePlayer1 == 'stone' && movePlayer2 == 'paper') {
+    if (movePlayer1 === 'stone' && movePlayer2 === 'paper') {
         winner = 2
-    } else if (movePlayer1 == 'stone' && movePlayer2 == 'scissors') {
+    } else if (movePlayer1 === 'stone' && movePlayer2 === 'scissors') {
         winner = 1
-    } else if (movePlayer1 == 'paper' && movePlayer2 == 'stone') {
+    } else if (movePlayer1 === 'paper' && movePlayer2 === 'stone') {
         winner = 1
-    } else if (movePlayer1 == 'paper' && movePlayer2 == 'scissors') {
+    } else if (movePlayer1 === 'paper' && movePlayer2 === 'scissors') {
         winner = 2
-    } else if (movePlayer1 == 'scissors' && movePlayer2 == 'stone') {
+    } else if (movePlayer1 === 'scissors' && movePlayer2 === 'stone') {
         winner = 2
-    } else if (movePlayer1 == 'scissors' && movePlayer2 == 'paper') {
+    } else if (movePlayer1 === 'scissors' && movePlayer2 === 'paper') {
         winner = 1
-    } else if (movePlayer1 == movePlayer2) {
+    } else if (movePlayer1 === movePlayer2) {
         winner = 0
     }
 }
 
 function printWinnerName(winner) {
-    if (winner == 1) {
+    if (winner === 1) {
         $winnerTitle.innerHTML = 'Jogador 1 ganhou!'
-    } else if (winner == 2) {
+    } else if (winner === 2) {
         $winnerTitle.innerHTML = 'Jogador 2 ganhou!'
     } else {
         $winnerTitle.innerHTML = 'Empatou'
     }
 }
 
-function givePoint(winner){
-    if (winner == 1) {
-        scorePlayer1 = scorePlayer1 + 1
-    } else if(winner == 2) {
-        scorePlayer2 = scorePlayer2 +1
-    }    
+function givePoint(winner) {
+    if (winner === 1) {
+        scorePlayer1++
+    } else if (winner === 2) {
+        scorePlayer2++
+    }
 }
 
-function printPoint(){
+function printPoint() {
     $scoreBoard1.innerHTML = scorePlayer1
     $scoreBoard2.innerHTML = scorePlayer2
 }
 
-function resetBoards(){
+function resetBoards() {
     $fieldPlayer1.innerHTML = ''
     $fieldPlayer2.innerHTML = ''
 }
 
-function resetVariables(){
+function resetVariables() {
     movePlayer1 = ''
     movePlayer2 = ''
+    winner = null
 }
 
-function resetScore(){
-    $scoreBoard1.innerHTML = '0'
-    $scoreBoard2.innerHTML = '0'
+function resetScore() {
+    scorePlayer1 = 0
+    scorePlayer2 = 0
+    printPoint()
 }
 
-function resetWinnerTitle(){
+function resetWinnerTitle() {
     $winnerTitle.innerHTML = 'Vencedor'
 }
 
-function resetScorePlayerVariables(){
-    scorePlayer1 = 0
-    scorePlayer1 = 0
-}
-
-function resetAll(){
+function resetAll() {
     resetVariables()
     resetBoards()
     resetScore()
     resetWinnerTitle()
-    resetScorePlayerVariables()
 }
 
-$buttonStone1.addEventListener('click', function (){
-    if (gameStart == false) return
+$buttonStone1.addEventListener('click', function () {
+    if (!gameStart || movePlayer1 || movePlayer2) return
     $fieldPlayer1.innerHTML = '<img class="image-move" src="./images/stone.png"/>'
     movePlayer1 = 'stone'
-    verifyWinner()
-    if (winner != null) {
-        printWinnerName(winner)
-        givePoint(winner)
-        printPoint()
-        resetVariables
-        setTimeout(resetBoards, 1000)
-    }
+    checkAndResolveGame()
 })
 
 $buttonPaper1.addEventListener('click', function () {
-    if (gameStart == false) return
+    if (!gameStart || movePlayer1 || movePlayer2) return
     $fieldPlayer1.innerHTML = '<img class="image-move" src="./images/paper.png"/>'
     movePlayer1 = 'paper'
-    verifyWinner()
-    if (winner != null) {
-        printWinnerName(winner)
-        givePoint(winner)
-        printPoint()
-        resetVariables
-        setTimeout(resetBoards, 1000)
-    }
+    checkAndResolveGame()
 })
 
 $buttonScissors1.addEventListener('click', function () {
-    if (gameStart == false) return
+    if (!gameStart || movePlayer1 || movePlayer2) return
     $fieldPlayer1.innerHTML = '<img class="image-move" src="./images/scissors.png"/>'
     movePlayer1 = 'scissors'
-    verifyWinner()
-    if (winner != null) {
-        printWinnerName(winner)
-        givePoint(winner)
-        printPoint()
-        resetVariables
-        setTimeout(resetBoards, 1000)
-    }
+    checkAndResolveGame()
 })
 
 $buttonStone2.addEventListener('click', function () {
-    if (gameStart == false) return
+    if (!gameStart || movePlayer1 === '' || movePlayer2) return
     $fieldPlayer2.innerHTML = '<img class="image-move" src="./images/stone.png"/>'
     movePlayer2 = 'stone'
-    verifyWinner()
-    if (winner != null) {
-        printWinnerName(winner)
-        givePoint(winner)
-        printPoint()
-        resetVariables
-        setTimeout(resetBoards, 1000)
-    }
+    checkAndResolveGame()
 })
 
 $buttonPaper2.addEventListener('click', function () {
-    if (gameStart == false) return
+    if (!gameStart || movePlayer1 === '' || movePlayer2) return
     $fieldPlayer2.innerHTML = '<img class="image-move" src="./images/paper.png"/>'
     movePlayer2 = 'paper'
-    verifyWinner()
-    if (winner != null) {
-        printWinnerName(winner)
-        givePoint(winner)
-        printPoint()
-        resetVariables
-        setTimeout(resetBoards, 1000) 
-    }
+    checkAndResolveGame()
 })
 
 $buttonScissors2.addEventListener('click', function () {
-    if (gameStart == false) return
+    if (!gameStart || movePlayer1 === '' || movePlayer2) return
     $fieldPlayer2.innerHTML = '<img class="image-move" src="./images/scissors.png"/>'
     movePlayer2 = 'scissors'
-    verifyWinner()
-    if (winner != null) {
-        printWinnerName(winner)
-        givePoint(winner)
-        printPoint()
-        resetVariables
-        setTimeout(resetBoards, 1000)
-    }
+    checkAndResolveGame()
 })
 
 $buttonReset.addEventListener('click', resetAll)
 
-$buttonStart.addEventListener('click', function(){
-    if (gameStart == false) {
+$buttonStart.addEventListener('click', function () {
+    if (!gameStart) {
         gameStart = true
         $buttonStart.classList.add('button-active')
         $buttonStart.innerHTML = 'Parar'
     } else {
         gameStart = false
-    gameStart = true
-    $buttonStart.classList.add('button-active')
-    $buttonStart.innerHTML = 'Iniciar'
+        $buttonStart.classList.remove('button-active')
+        $buttonStart.innerHTML = 'Iniciar'
     }
 })
+
+function checkAndResolveGame() {
+    if (movePlayer1 && movePlayer2) {
+        verifyWinner()
+        if (winner !== null) {
+            printWinnerName(winner)
+            givePoint(winner)
+            printPoint()
+            setTimeout(() => {
+                resetBoards()
+                resetVariables()
+            }, 1000)
+        }
+    }
+}
